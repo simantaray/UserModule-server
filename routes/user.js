17a67c48acfc3c0a909be6742537df881df3f499
simantaray <simantaray89@gmail.com>
2022-06-03 11:30:37 +0530

    fix jwt ErrorHandle

--- routes/user.js
+++ routes/user.js
@@ -1,27 +1,28 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const JWT = require("jsonwebtoken");
 
 const { verifyAdmin } = require("./verifyRoles");
 
 router.post("/register", async (req, res) => {
   const passwordHash = bcrypt.hashSync(req.body.password, 10);
-  // console.log(bcrypt.compareSync('Pa$$w0rd', "$2a$10$uRMuqwJergg.c5okh.hHWOTkYkXaQBrDOQQdjPCiot.0qnlljBb/W"))
-
+  //needed to handel duplication of username
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
     password: passwordHash,
   });
-  res.json(newUser);
-
-  //   console.log(newUser);
-  //   try {
-  //     const response = await newUser.save();
-  //     res.status(201).json(response);
-  //   } catch (e) {
-  //     res.status(500).json(e);
-  //   }
+  try {
+    const response = await newUser.save();
+    const { password, ...rest } = response._doc;
+    res.status(201).json({...rest});
+  } catch (e) {
+    if(e.code=="11000"){
+      res.status(400).json({status: "username already exist"});
+    }
+    res.status(500).json(e);
+  }
 });
 
 router.post("/login", async (req, res) => {
@@ -30,11 +31,19 @@ router.post("/login", async (req, res) => {
     if (!response) {
       res.status(401).json("u r not a user");
     } else {
-      if (response.password !== req.body.password) {
+      if (!bcrypt.compareSync(req.body.password, response.password)) {
         res.status(401).json("wrong password");
       } else {
         const { password, roles, ...rest } = response._doc;
-        res.status(200).json(roles);
+        const acessToken = JWT.sign(
+          {
+            id: response.id,
+            role: response.roles,
+          },
+          process.env.PASSTOKEN,
+          { expiresIn: "2d" }
+        );
+        res.status(200).json({ ...rest, acessToken });
       }
     }
   } catch (err) {
@@ -62,17 +71,37 @@ router.put("/:id", verifyAdmin, async (req, res) => {
 });
 //add  new role
 router.post("/addrole/:role/:id", verifyAdmin, async (req, res) => {
-  try {
-    const response = await User.findOneAndUpdate(
-      { _id: req.params.id },
-      { $addToSet: { roles: { $each: [req.params.role] } } },
-      { new: true }
-    );
-    if (response) res.status(200).json({ status: "role added" });
-  } catch (err) {
-    res.status(500).json(err);
+  let assignRole;
+  switch (req.params.role) {
+    case "0":
+      assignRole = "admin";
+      break;
+    case "1":
+      assignRole = "hr";
+      break;
+    case "2":
+      assignRole = "teamleader";
+      break;
+    case "3":
+      assignRole = "teammember";
+      break;
+    default:
+      assignRole = "notValid";
   }
+  if (assignRole != "notValid") {
+    try {
+      const response = await User.findOneAndUpdate(
+        { _id: req.params.id },
+        { $addToSet: { roles: { $each: [assignRole] } } },
+        { new: true }
+      );
+      if (response) res.status(200).json({ status: "role added" });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else res.json({ status: "not a valid role" });
 });
+
 //remove role
 router.post("/removerole/:role/:id", verifyAdmin, async (req, res) => {
   try {
