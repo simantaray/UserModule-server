10ee8dec3ce9132e23f09286e86ed799e29438cb
simantaray <simantaray89@gmail.com>
2022-06-03 11:10:09 +0530

    fix verify With Jwt

--- routes/verifyRoles.js
+++ routes/verifyRoles.js
@@ -1,31 +1,31 @@
 //admin role verify
+const JWT = require("jsonwebtoken");
 const verifyAdmin = (req, res, next) => {
-  const roleHeader = req.headers.role;
-  if (roleHeader) {
-    if (roleHeader == "admin") {
-      req.userRole = "admin";
-      next();
-    } else {
-      return res.status(401).json("u are not admin");
-    }
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    JWT.verify(token, process.env.PASSTOKEN, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      if (user.role.includes("admin")) next();
+    });
   } else {
     return res.status(401).json("u dont have any role");
   }
 };
 const verifyTeamLeader = (req, res, next) => {
-    const roleHeader = req.headers.role;
-    if (roleHeader) {
-      if (roleHeader == "teamleader") {
-        req.userRole = "teamleader";
-        next();
-      } else {
-        return res.status(401).json("u are not teamleader");
-      }
-    } else {
-      return res.status(401).json("u dont have any role");
-    }
-  };
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    JWT.verify(token, process.env.PASSTOKEN, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      if (user.role.includes("teamleader")) next();
+    });
+  } else {
+    return res.status(401).json("u dont have any role");
+  }
+};
 
 module.exports = {
-    verifyAdmin , verifyTeamLeader
+  verifyAdmin,
+  verifyTeamLeader,
 };
